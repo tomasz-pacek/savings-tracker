014f9d47bd972263bf7ecb78d2adfca14c960d86
nieukki <tomekpacek@o2.pl>
2026-06-30 10:38:36 +0200

    auth-client baseURL fix

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,13 @@
 import { createAuthClient } from "better-auth/react";
+
+const baseURL =
+  process.env.NODE_ENV === "development"
+    ? "https://localhost:3000"
+    : typeof window !== "undefined"
+      ? window.location.origin
+      : (process.env.NEXT_PUBLIC_SITE_URL as string);
+
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: "http://localhost:3000",
+  baseURL,
 });
